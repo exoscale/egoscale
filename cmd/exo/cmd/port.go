540ad3767216aba18723843f0fdfaf7119fe202c
Yoan Blanc <yoan.blanc@exoscale.ch>
2018-06-08 09:32:43 +0200

    fixup! exo: make the root command public
    Signed-off-by: Yoan Blanc <yoan.blanc@exoscale.ch>

--- cmd/exo/cmd/port.go
+++ cmd/exo/cmd/port.go
@@ -9,26 +9,47 @@ import (
 type port uint16
 
 const (
-	Daytime   port = 13
-	FTP       port = 21
-	SSH       port = 22
-	Telnet    port = 23
-	SMTP      port = 25
-	Time      port = 37
-	Whois     port = 43
-	DNS       port = 53
-	TFTP      port = 69
-	Gopher    port = 70
-	HTTP      port = 80
-	Kerberos  port = 88
-	Nic       port = 101
-	SFTP      port = 115
-	NTP       port = 123
-	IMAP      port = 143
-	SNMP      port = 161
-	IRC       port = 194
-	HTTPS     port = 443
-	RDP       port = 3389
+	// Daytime of the night
+	Daytime port = 13
+	// FTP protocol port number
+	FTP port = 21
+	// SSH protocol port number
+	SSH port = 22
+	// Telnet protocol port number
+	Telnet port = 23
+	// SMTP protocol port number
+	SMTP port = 25
+	// Time protocol port number
+	Time port = 37
+	// Whois protocol port number
+	Whois port = 43
+	// DNS protocol port number
+	DNS port = 53
+	// TFTP protocol port number
+	TFTP port = 69
+	// Gopher protocol port number
+	Gopher port = 70
+	// HTTP protocol port number
+	HTTP port = 80
+	// Kerberos protocol port number
+	Kerberos port = 88
+	// Nic protocol port number
+	Nic port = 101
+	// SFTP protocol port number
+	SFTP port = 115
+	// NTP protocol port number
+	NTP port = 123
+	// IMAP protocol port number
+	IMAP port = 143
+	// SNMP protocol port number
+	SNMP port = 161
+	// IRC protocol port number
+	IRC port = 194
+	// HTTPS protocol port number
+	HTTPS port = 443
+	// RDP protocol port number
+	RDP port = 3389
+	// Minecraft protocol port number
 	Minecraft port = 25565
 )
 

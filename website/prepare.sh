498ceb8144912fc2bc124ddf2071c2e815edb8d2
Yoan Blanc <yoan.blanc@exoscale.ch>
2018-06-14 11:42:50 +0200

    website: bulma style
    Signed-off-by: Yoan Blanc <yoan.blanc@exoscale.ch>

--- website/prepare.sh
+++ website/prepare.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -xe
 
+cp ../README.md content/_index.md
+cp ../cmd/cs/README.md content/cs/_index.md
+cp ../cmd/exo/README.md content/cli/_index.md
+mkdir -p static
+cp ../gopher.png static
+
 cd ../
 dep ensure -vendor-only
 
@@ -8,7 +14,6 @@ cd cmd/cs
 dep ensure -vendor-only
 go build
 ./cs gen-doc
-cp README.md ../../website/content/cs/_index.md
 
 cd ../exo
 dep ensure -vendor-only

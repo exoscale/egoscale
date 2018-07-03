b7bbe6d8477e618c5c5ee34be66944b0cdcdd6f0
Pierre-Emmanuel Jacquier <pierre-emmanuel.jacquier@epitech.eu>
2018-07-03 21:28:27 +0200

    fixup! Add multiple associate and dissociate
    Signed-off-by: Pierre-Emmanuel Jacquier <pierre-emmanuel.jacquier@epitech.eu>

--- cmd/exo/cmd/eip_associate.go
+++ cmd/exo/cmd/eip_associate.go
@@ -3,10 +3,8 @@ package cmd
 import (
 	"fmt"
 	"net"
-	"os"
 
 	"github.com/exoscale/egoscale"
-	"github.com/exoscale/egoscale/cmd/exo/table"
 
 	"github.com/spf13/cobra"
 )
@@ -14,53 +12,48 @@ import (
 // associateCmd represents the associate command
 var eipAssociateCmd = &cobra.Command{
 	Use:   "associate <IP address> <instance name | instance id> [instance name | instance id] [...]",
-	Short: "Associate an IP to an instance",
+	Short: "Associate an IP to instance(s)",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		if len(args) < 2 {
 			return cmd.Usage()
 		}
 
 		for _, arg := range args[1:] {
-			if err := associateIP(args[0], arg); err != nil {
+			res, err := associateIP(args[0], arg)
+			if err != nil {
 				return err
 			}
+			println(res)
 		}
 		return nil
 	},
 }
 
-func associateIP(ipAddr, instance string) error {
+func associateIP(ipAddr, instance string) (string, error) {
 	ip := net.ParseIP(ipAddr)
 	if ip == nil {
-		return fmt.Errorf("Invalid IP address")
+		return "", fmt.Errorf("Invalid IP address")
 	}
 
 	vm, err := getVMWithNameOrID(cs, instance)
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	defaultNic := vm.DefaultNic()
 
 	if defaultNic == nil {
-		return fmt.Errorf("No default NIC on this instance")
+		return "", fmt.Errorf("No default NIC on this instance")
 	}
 
 	resp, err := cs.Request(&egoscale.AddIPToNic{NicID: defaultNic.ID, IPAddress: ip})
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	result := resp.(*egoscale.NicSecondaryIP)
 
-	table := table.NewTable(os.Stdout)
-	table.SetHeader([]string{"Virtual machine", "IP", "Secondary IP"})
-
-	table.Append([]string{vm.Name, vm.IP().String(), result.IPAddress.String()})
-
-	table.Render()
-
-	return nil
+	return result.NicID, nil
 }
 
 func init() {

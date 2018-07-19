3d976cbf41b90fc0b5a4b492374ffb0e289a4139
Pierre-Emmanuel Jacquier <pierre-emmanuel.jacquier@epitech.eu>
2018-07-19 16:39:12 +0200

    Add meta and header base cmd
    Signed-off-by: Pierre-Emmanuel Jacquier <pierre-emmanuel.jacquier@epitech.eu>

---
+++ cmd/exo/cmd/sos_headers.go
@@ -0,0 +1,56 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// headersCmd represents the headers command
+var sosHeadersCmd = &cobra.Command{
+	Use:   "header",
+	Short: "Add or remove an header key/value from an object",
+}
+
+func init() {
+	sosCmd.AddCommand(sosHeadersCmd)
+}
+
+// headersCmd represents the headers command
+var sosAddHeadersCmd = &cobra.Command{
+	Use:   "add",
+	Short: "Add an header key/value to an object",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("headers called")
+	},
+}
+
+func init() {
+	sosHeadersCmd.AddCommand(sosAddHeadersCmd)
+}
+
+// headersCmd represents the headers command
+var sosRemoveHeadersCmd = &cobra.Command{
+	Use:   "remove",
+	Short: "Remove an header key/value from an object",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("headers called")
+	},
+}
+
+func init() {
+	sosHeadersCmd.AddCommand(sosRemoveHeadersCmd)
+}
+
+// headersCmd represents the headers command
+var sosShowHeadersCmd = &cobra.Command{
+	Use:   "show",
+	Short: "Show object headers",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("headers called")
+	},
+}
+
+func init() {
+	sosHeadersCmd.AddCommand(sosShowHeadersCmd)
+}
---
+++ cmd/exo/cmd/sos_metadata.go
@@ -0,0 +1,58 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// metadataCmd represents the metadata command
+var sosMetadataCmd = &cobra.Command{
+	Use:   "metadata",
+	Short: "Add and remove metadata from an object",
+}
+
+func init() {
+	sosCmd.AddCommand(sosMetadataCmd)
+}
+
+// metadataCmd represents the metadata command
+var sosAddMetadataCmd = &cobra.Command{
+	Use:   "add",
+	Short: "Add a metadata to an object",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("metadata called")
+	},
+}
+
+func init() {
+	sosMetadataCmd.AddCommand(sosAddMetadataCmd)
+}
+
+// metadataCmd represents the metadata command
+var sosRemoveMetadataCmd = &cobra.Command{
+	Use:     "remove",
+	Aliases: gRemoveAlias,
+	Short:   "Remove a metadata from an object",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("metadata called")
+	},
+}
+
+func init() {
+	sosMetadataCmd.AddCommand(sosRemoveMetadataCmd)
+}
+
+// metadataCmd represents the metadata command
+var sosShowMetadataCmd = &cobra.Command{
+	Use:     "show",
+	Short:   "Show object metadatas",
+	Aliases: gShowAlias,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Show object metadatas")
+	},
+}
+
+func init() {
+	sosMetadataCmd.AddCommand(sosShowMetadataCmd)
+}

3824c1942505279b6ff06cfca7fde548dd44fa80
Pierre-Emmanuel Jacquier <pierre-emmanuel.jacquier@epitech.eu>
2018-08-03 10:22:24 +0200

    Move vm methode in exo
    Signed-off-by: Pierre-Emmanuel Jacquier <pierre-emmanuel.jacquier@epitech.eu>

--- cmd/exo/cmd/vm_stop.go
+++ cmd/exo/cmd/vm_stop.go
@@ -1,9 +1,11 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
 	"os"
 
+	"github.com/exoscale/egoscale"
 	"github.com/spf13/cobra"
 )
 
@@ -24,7 +26,7 @@ var vmStopCmd = &cobra.Command{
 			}
 
 			fmt.Printf("Stoping %q ", vm.Name)
-			if err := vm.AsyncStopWithCtx(gContext, *cs, true); err != nil {
+			if err := vm.asyncStopWithCtx(gContext, *cs, true); err != nil {
 				fmt.Fprintln(os.Stderr, err)
 			} else {
 				fmt.Println("\nstopped !")
@@ -32,6 +34,46 @@ var vmStopCmd = &cobra.Command{
 		}
 		return nil
 	},
+}
+
+// Stop a virtual machine instance
+func (vm virtualMachine) stop(cs egoscale.Client) error {
+	return vm.stopWithCtx(context.Background(), cs)
+}
+
+// StopWithCtx a virtual machine instance
+func (vm virtualMachine) stopWithCtx(ctx context.Context, cs egoscale.Client) error {
+	_, err := cs.RequestWithContext(ctx, &egoscale.StopVirtualMachine{ID: vm.ID})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// AsyncStop stop a virtual machine instance Async
+func (vm virtualMachine) asyncStop(cs egoscale.Client, displayLoad bool) error {
+	return vm.asyncStopWithCtx(context.Background(), cs, displayLoad)
+}
+
+// AsyncStopWithCtx stop a virtual machine instance Async
+func (vm virtualMachine) asyncStopWithCtx(ctx context.Context, cs egoscale.Client, displayLoad bool) error {
+	var errorReq error
+	cs.AsyncRequest(&egoscale.StopVirtualMachine{ID: vm.ID}, func(jobResult *egoscale.AsyncJobResult, err error) bool {
+		if displayLoad {
+			fmt.Printf(".")
+		}
+
+		if err != nil {
+			errorReq = err
+			return false
+		}
+
+		if jobResult.JobStatus == egoscale.Success {
+			return false
+		}
+		return true
+	})
+	return errorReq
 }
 
 func init() {

e9737f3b934e4d73c7f54bf8afb7245baa704c87
Pierre-Emmanuel Jacquier <pierre-emmanuel.jacquier@epitech.eu>
2018-08-03 13:47:48 +0200

    implem exo vm scale
    Signed-off-by: Pierre-Emmanuel Jacquier <pierre-emmanuel.jacquier@epitech.eu>

---
+++ cmd/exo/cmd/vm_scale.go
@@ -0,0 +1,106 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/exoscale/egoscale"
+
+	"github.com/spf13/cobra"
+)
+
+// scaleCmd represents the scale command
+var vmScaleCmd = &cobra.Command{
+	Use:   "scale <vm name> [vm name] ...",
+	Short: "Scale virtual machine",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if len(args) < 1 {
+			return cmd.Usage()
+		}
+
+		so, err := cmd.Flags().GetString("service-offering")
+		if err != nil {
+			return err
+		}
+
+		serviceoffering, err := getServiceOfferingByName(cs, so)
+		if err != nil {
+			return err
+		}
+
+		for _, v := range args {
+			vm, err := getVMWithNameOrID(cs, v)
+			if err != nil {
+				return err
+			}
+
+			fmt.Printf("Scaling %q ", vm.Name)
+			if err := vm.asyncScaleWithCtx(gContext, *cs, serviceoffering, true); err != nil {
+				fmt.Fprintf(os.Stderr, "\n%v\n", err)
+			} else {
+				fmt.Println("\nScaled !")
+			}
+		}
+		return nil
+	},
+}
+
+// Scale a virtual machine instance
+func (vm virtualMachine) scale(cs egoscale.Client, so *egoscale.ServiceOffering) error {
+	return vm.scaleWithCtx(context.Background(), cs, so)
+}
+
+// ScaleWithCtx a virtual machine instance
+func (vm virtualMachine) scaleWithCtx(ctx context.Context, cs egoscale.Client, so *egoscale.ServiceOffering) error {
+
+	if vm.State != stopped {
+		return fmt.Errorf("This operation is not permitted if your VM is running")
+	}
+
+	_, err := cs.RequestWithContext(ctx, &egoscale.ScaleVirtualMachine{ID: vm.ID, ServiceOfferingID: so.ID})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// AsyncScale scale a virtual machine instance Async
+func (vm virtualMachine) asyncScale(cs egoscale.Client, so *egoscale.ServiceOffering, displayLoad bool) error {
+	return vm.asyncScaleWithCtx(context.Background(), cs, so, displayLoad)
+}
+
+// AsyncscaleWithCtx scale a virtual machine instance Async with context
+func (vm virtualMachine) asyncScaleWithCtx(ctx context.Context, cs egoscale.Client, so *egoscale.ServiceOffering, displayLoad bool) error {
+
+	if vm.State != stopped {
+		return fmt.Errorf("This operation is not permitted if your VM is running")
+	}
+
+	var errorReq error
+	cs.AsyncRequest(&egoscale.ScaleVirtualMachine{ID: vm.ID, ServiceOfferingID: so.ID}, func(jobResult *egoscale.AsyncJobResult, err error) bool {
+		if displayLoad {
+			fmt.Printf(".")
+		}
+
+		if err != nil {
+			errorReq = err
+			return false
+		}
+
+		if jobResult.JobStatus == egoscale.Success {
+			return false
+		}
+		return true
+	})
+	return errorReq
+}
+
+func init() {
+	vmCmd.AddCommand(vmScaleCmd)
+	vmScaleCmd.Flags().StringP("service-offering", "o", "", "<name | id> (micro|tiny|small|medium|large|extra-large|huge|mega|titan")
+	if err := vmScaleCmd.MarkFlagRequired("service-offering"); err != nil {
+		log.Fatal(err)
+	}
+}

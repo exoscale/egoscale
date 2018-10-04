2c325e89082e488fa7e2d7c315cf920d24c63342
mcorbin <corbin.math@gmail.com>
2018-10-04 12:42:43 +0200

    privnet associate: add an optional ip parameter

--- cmd/exo/cmd/privnet_associate.go
+++ cmd/exo/cmd/privnet_associate.go
@@ -2,14 +2,17 @@ package cmd
 
 import (
 	"fmt"
+	"net"
+	"os"
 
 	"github.com/exoscale/egoscale"
+	"github.com/exoscale/egoscale/cmd/exo/table"
 	"github.com/spf13/cobra"
 )
 
 // privnetAssociateCmd represents the associate command
 var privnetAssociateCmd = &cobra.Command{
-	Use:     "associate <privnet name | id> <vm name | vm id> [vm name | vm id] [...]",
+	Use:     "associate <privnet name | id> <vm name | vm id> [<ip>] [<vm name | vm id> [<ip>] [...]",
 	Short:   "Associate a private network to instance(s)",
 	Aliases: gAssociateAlias,
 	RunE: func(cmd *cobra.Command, args []string) error {
@@ -21,26 +24,47 @@ var privnetAssociateCmd = &cobra.Command{
 		if err != nil {
 			return err
 		}
-
-		for _, vm := range args[1:] {
-			resp, err := associatePrivNet(network, vm)
+		table := table.NewTable(os.Stdout)
+		table.SetHeader([]string{"Nic ID", "Virtual Machine ID", "IP Address"})
+		for i := 1; i < len(args); i++ {
+			name := args[i]
+			if i != len(args)-1 {
+				ip := net.ParseIP(args[i+1])
+				if ip != nil {
+					// the next param is an ip
+					nic, err := associatePrivNet(network, name, ip)
+					if err != nil {
+						return err
+					}
+					table.Append([]string{
+						nic.ID.String(),
+						nic.VirtualMachineID.String(),
+						nicIP(nic)})
+					i = i + 1
+					continue
+				}
+			}
+			nic, err := associatePrivNet(network, name, nil)
 			if err != nil {
 				return err
 			}
-			fmt.Println(resp)
+			table.Append([]string{
+				nic.ID.String(),
+				nic.VirtualMachineID.String(),
+				nicIP(nic)})
 		}
-
+		table.Render()
 		return nil
 	},
 }
 
-func associatePrivNet(privnet *egoscale.Network, vmName string) (*egoscale.UUID, error) {
+func associatePrivNet(privnet *egoscale.Network, vmName string, ip net.IP) (*egoscale.Nic, error) {
 	vm, err := getVMWithNameOrID(vmName)
 	if err != nil {
 		return nil, err
 	}
 
-	req := &egoscale.AddNicToVirtualMachine{NetworkID: privnet.ID, VirtualMachineID: vm.ID}
+	req := &egoscale.AddNicToVirtualMachine{NetworkID: privnet.ID, VirtualMachineID: vm.ID, IPAddress: ip}
 	resp, err := cs.RequestWithContext(gContext, req)
 	if err != nil {
 		return nil, err
@@ -51,7 +75,7 @@ func associatePrivNet(privnet *egoscale.Network, vmName string) (*egoscale.UUID,
 		return nil, fmt.Errorf("no nics found for network %q", privnet.ID)
 	}
 
-	return nic.ID, nil
+	return nic, nil
 
 }
 

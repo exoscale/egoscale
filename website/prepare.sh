db14fb8fb983e5f76257148a3a66028caa569795
Yoan Blanc <yoan.blanc@exoscale.ch>
2018-10-12 11:40:40 +0200

    exo: remove it
    - new releases will only be tarballs
    - the website won't change (for the time being)
    
    Signed-off-by: Yoan Blanc <yoan.blanc@exoscale.ch>

--- website/prepare.sh
+++ website/prepare.sh
@@ -3,7 +3,6 @@ set -xe
 
 cp ../README.md content/_index.md
 cp ../cmd/cs/README.md content/cs/_index.md
-cp ../cmd/exo/README.md content/cli/_index.md
 mkdir -p static
 cp ../gopher.png static
 
@@ -14,10 +13,6 @@ cd cmd/cs
 dep ensure -vendor-only
 go build
 ./cs gen-doc
-
-cd ../exo
-dep ensure -vendor-only
-go run doc/main.go
 
 set +xe
 echo "we are now ready to: hugo serve"

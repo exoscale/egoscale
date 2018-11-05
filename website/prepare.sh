46f69d8e547759aad71a97243a7c6d3b9f4e844e
Yoan Blanc <yoan@dosimple.ch>
2018-11-05 11:06:03 +0100

    cs: move elsewhere (#357)
    Signed-off-by: Yoan Blanc <yoan.blanc@exoscale.ch>
    Reviewed-by: Pierre-Yves Ritschard <pyr@spootnik.org>

--- website/prepare.sh
+++ website/prepare.sh
@@ -2,17 +2,8 @@
 set -xe
 
 cp ../README.md content/_index.md
-cp ../cmd/cs/README.md content/cs/_index.md
 mkdir -p static
 cp ../gopher.png static
-
-cd ../
-dep ensure -vendor-only
-
-cd cmd/cs
-dep ensure -vendor-only
-go build
-./cs gen-doc
 
 set +xe
 echo "we are now ready to: hugo serve"

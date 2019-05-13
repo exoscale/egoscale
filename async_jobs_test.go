21b7a5f1158d438882c8c809f0042424faade026
Pierre-Emmanuel Jacquier <pierre-emmanuel.jacquier@epitech.eu>
2019-05-13 12:48:45 +0000

    Add test
    Signed-off-by: Pierre-Emmanuel Jacquier <pierre-emmanuel.jacquier@epitech.eu>

--- async_jobs_test.go
+++ async_jobs_test.go
@@ -13,3 +13,34 @@ func TestListAsyncJobs(t *testing.T) {
 	req := &ListAsyncJobs{}
 	_ = req.Response().(*ListAsyncJobsResponse)
 }
+
+func TestAsyncJobsResultDeepCopy(t *testing.T) {
+	req := &AsyncJobResult{
+		JobID: MustParseUUID("5d99e658-d0d4-4a91-b194-fc8bb44c272e"),
+	}
+	copy := req.DeepCopy()
+
+	if copy.JobID == nil {
+		t.Errorf("JobID non nil is expected")
+	}
+
+	if !copy.JobID.Equal(*MustParseUUID("5d99e658-d0d4-4a91-b194-fc8bb44c272e")) {
+		t.Errorf(`uuid is not equal to "5d99e658-d0d4-4a91-b194-fc8bb44c272e": got %q`, copy.JobID.String())
+	}
+}
+
+func TestAsyncJobsResultDeepCopyInto(t *testing.T) {
+	req := &AsyncJobResult{
+		JobID: MustParseUUID("5d99e658-d0d4-4a91-b194-fc8bb44c272e"),
+	}
+	copy := new(AsyncJobResult)
+	req.DeepCopyInto(copy)
+
+	if copy.JobID == nil {
+		t.Errorf("JobID non nil is expected")
+	}
+
+	if !copy.JobID.Equal(*MustParseUUID("5d99e658-d0d4-4a91-b194-fc8bb44c272e")) {
+		t.Errorf(`uuid is not equal to "5d99e658-d0d4-4a91-b194-fc8bb44c272e": got %q`, copy.JobID.String())
+	}
+}

1df9bbb653f7e49210723d9e63f31743cb97083f
Pierre-Emmanuel Jacquier <15922119+pierre-emmanuelJ@users.noreply.github.com>
2019-12-04 13:24:03 +0000

    Add const ComputeAPIEndpoint and DNSAPIEndpoint
    Signed-off-by: Pierre-Emmanuel Jacquier <15922119+pierre-emmanuelJ@users.noreply.github.com>

--- version.go
+++ version.go
@@ -2,3 +2,12 @@ package egoscale
 
 // Version of the library
 const Version = "0.21.0"
+
+const (
+	computeAPIVersion = "v1"
+	// ComputeAPIEndpoint is the Exoscale Compute endpoint with the latest API version.
+	ComputeAPIEndpoint = "https://api.exoscale.com/" + computeAPIVersion
+
+	// DNSAPIEndpoint is the Exoscale API DNS endpoint.
+	DNSAPIEndpoint = "https://api.exoscale.com/dns"
+)

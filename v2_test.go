09bc55d577b2a198d97c20b09705f26b21403da4
Johnatan Illi <johnatan.illi@exoscale.ch>
2020-04-29 14:19:00 +0200

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! Upgrade to Go 1.14

--- v2_test.go
+++ v2_test.go
@@ -3,11 +3,14 @@ package egoscale
 import (
 	"context"
 	"testing"
+	"time"
 )
 
 func TestClient_Ping(t *testing.T) {
 	c := NewClient("https://api-ch-gva-2.exoscale.com/v2.alpha", "KEY", "SECRET")
-	if err := c.Ping(context.Background()); err != nil {
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Minute)
+	defer cancel()
+	if err := c.Ping(ctx); err != nil {
 		t.Fatalf("ping failed: %s", err)
 	}
 }

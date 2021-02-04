aed978c6b735ebbde21126c76e8554c4b71e684e
Marc Falzon <marc@exoscale.com>
2021-02-04 09:55:10 +0100

    Add Anti-Affinity Groups support to SKS Nodepools
    This changes adds support for Anti-Affinity Groups to SKS Nodepools.

--- internal/v2/sks_nodepool_test.go
+++ internal/v2/sks_nodepool_test.go
@@ -11,37 +11,40 @@ import (
 
 func TestSksNodepool_UnmarshalJSON(t *testing.T) {
 	var (
-		testID                    = "c19542b7-d269-4bd4-bf7c-2cae36d066d3"
-		testName                  = "test-nodepool"
-		testCreatedAt, _          = time.Parse(iso8601Format, "2020-08-12T11:12:36Z")
-		testDescription           = "Test Nodepool description"
-		testSize            int64 = 3
-		testDiskSize        int64 = 15
-		testInstancePoolID        = "f1f67118-43b6-4632-a709-d55fada62f21"
-		testInstanceTypeID        = "21624abb-764e-4def-81d7-9fc54b5957fb"
-		testSecurityGroupID       = "efb4f4df-87ce-44e9-b5ee-59a9c1628edf"
-		testState                 = "running"
-		testTemplateID            = "f270d9a2-db64-4e8e-9cd3-5125887e91aa"
-		testVersion               = "1.18.6"
+		testAntiAffinityGroupID       = "a266eadc-1e5c-4b0a-a31c-1325d2060434"
+		testCreatedAt, _              = time.Parse(iso8601Format, "2020-08-12T11:12:36Z")
+		testDescription               = "Test Nodepool description"
+		testDiskSize            int64 = 15
+		testID                        = "c19542b7-d269-4bd4-bf7c-2cae36d066d3"
+		testInstancePoolID            = "f1f67118-43b6-4632-a709-d55fada62f21"
+		testInstanceTypeID            = "21624abb-764e-4def-81d7-9fc54b5957fb"
+		testName                      = "test-nodepool"
+		testSecurityGroupID           = "efb4f4df-87ce-44e9-b5ee-59a9c1628edf"
+		testSize                int64 = 3
+		testState                     = "running"
+		testTemplateID                = "f270d9a2-db64-4e8e-9cd3-5125887e91aa"
+		testVersion                   = "1.18.6"
 
 		expected = SksNodepool{
-			CreatedAt:      &testCreatedAt,
-			Description:    &testDescription,
-			DiskSize:       &testDiskSize,
-			Id:             &testID,
-			InstancePool:   &InstancePool{Id: &testInstancePoolID},
-			InstanceType:   &InstanceType{Id: &testInstanceTypeID},
-			Name:           &testName,
-			SecurityGroups: &[]SecurityGroup{{Id: &testSecurityGroupID}},
-			Size:           &testSize,
-			State:          &testState,
-			Template:       &Template{Id: &testTemplateID},
-			Version:        &testVersion,
+			AntiAffinityGroups: &[]AntiAffinityGroup{{Id: &testAntiAffinityGroupID}},
+			CreatedAt:          &testCreatedAt,
+			Description:        &testDescription,
+			DiskSize:           &testDiskSize,
+			Id:                 &testID,
+			InstancePool:       &InstancePool{Id: &testInstancePoolID},
+			InstanceType:       &InstanceType{Id: &testInstanceTypeID},
+			Name:               &testName,
+			SecurityGroups:     &[]SecurityGroup{{Id: &testSecurityGroupID}},
+			Size:               &testSize,
+			State:              &testState,
+			Template:           &Template{Id: &testTemplateID},
+			Version:            &testVersion,
 		}
 
 		actual SksNodepool
 
 		jsonSksNodepool = `{
+  "anti-affinity-groups": [{"id":"` + testAntiAffinityGroupID + `"}],
   "created-at": "` + testCreatedAt.Format(iso8601Format) + `",
   "description": "` + testDescription + `",
   "disk-size": ` + fmt.Sprint(testDiskSize) + `,
@@ -63,35 +66,38 @@ func TestSksNodepool_UnmarshalJSON(t *testing.T) {
 
 func TestSksNodepool_MarshalJSON(t *testing.T) {
 	var (
-		testID                    = "c19542b7-d269-4bd4-bf7c-2cae36d066d3"
-		testName                  = "test-nodepool"
-		testCreatedAt, _          = time.Parse(iso8601Format, "2020-08-12T11:12:36Z")
-		testDescription           = "Test Nodepool description"
-		testSize            int64 = 3
-		testDiskSize        int64 = 15
-		testInstancePoolID        = "f1f67118-43b6-4632-a709-d55fada62f21"
-		testInstanceTypeID        = "21624abb-764e-4def-81d7-9fc54b5957fb"
-		testSecurityGroupID       = "efb4f4df-87ce-44e9-b5ee-59a9c1628edf"
-		testState                 = "running"
-		testTemplateID            = "f270d9a2-db64-4e8e-9cd3-5125887e91aa"
-		testVersion               = "1.18.6"
+		testAntiAffinityGroupID       = "a266eadc-1e5c-4b0a-a31c-1325d2060434"
+		testCreatedAt, _              = time.Parse(iso8601Format, "2020-08-12T11:12:36Z")
+		testDescription               = "Test Nodepool description"
+		testDiskSize            int64 = 15
+		testID                        = "c19542b7-d269-4bd4-bf7c-2cae36d066d3"
+		testInstancePoolID            = "f1f67118-43b6-4632-a709-d55fada62f21"
+		testInstanceTypeID            = "21624abb-764e-4def-81d7-9fc54b5957fb"
+		testName                      = "test-nodepool"
+		testSecurityGroupID           = "efb4f4df-87ce-44e9-b5ee-59a9c1628edf"
+		testSize                int64 = 3
+		testState                     = "running"
+		testTemplateID                = "f270d9a2-db64-4e8e-9cd3-5125887e91aa"
+		testVersion                   = "1.18.6"
 
 		sksNodepool = SksNodepool{
-			CreatedAt:      &testCreatedAt,
-			Description:    &testDescription,
-			DiskSize:       &testDiskSize,
-			Id:             &testID,
-			InstancePool:   &InstancePool{Id: &testInstancePoolID},
-			InstanceType:   &InstanceType{Id: &testInstanceTypeID},
-			Name:           &testName,
-			SecurityGroups: &[]SecurityGroup{{Id: &testSecurityGroupID}},
-			Size:           &testSize,
-			State:          &testState,
-			Template:       &Template{Id: &testTemplateID},
-			Version:        &testVersion,
+			AntiAffinityGroups: &[]AntiAffinityGroup{{Id: &testAntiAffinityGroupID}},
+			CreatedAt:          &testCreatedAt,
+			Description:        &testDescription,
+			DiskSize:           &testDiskSize,
+			Id:                 &testID,
+			InstancePool:       &InstancePool{Id: &testInstancePoolID},
+			InstanceType:       &InstanceType{Id: &testInstanceTypeID},
+			Name:               &testName,
+			SecurityGroups:     &[]SecurityGroup{{Id: &testSecurityGroupID}},
+			Size:               &testSize,
+			State:              &testState,
+			Template:           &Template{Id: &testTemplateID},
+			Version:            &testVersion,
 		}
 
 		expected = []byte(`{` +
+			`"anti-affinity-groups":[{"id":"` + testAntiAffinityGroupID + `"}],` +
 			`"created-at":"` + testCreatedAt.Format(iso8601Format) + `",` +
 			`"description":"` + testDescription + `",` +
 			`"disk-size":` + fmt.Sprint(testDiskSize) + `,` +

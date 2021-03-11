13f18c622f9ad6a927b7633c510f38204892d88c
Marc Falzon <marc@exoscale.com>
2021-03-11 08:58:04 +0100

    SKS: add SKSCluster.RotateCCMCredentials()
    This change adds a new `RotateCCMCredentials()` method to the
    `SKSCluster` struct.

--- v2/sks.go
+++ v2/sks.go
@@ -112,6 +112,13 @@ func sksClusterFromAPI(c *papi.SksCluster) *SKSCluster {
 	}
 }
 
+// RotateCCMCredentials rotates the Exoscale IAM credentials managed by the SKS control plane for the
+// Kubernetes Exoscale Cloud Controller Manager.
+func (c *SKSCluster) RotateCCMCredentials(ctx context.Context) error {
+	_, err := c.c.RotateSksCcmCredentialsWithResponse(apiv2.WithZone(ctx, c.zone), c.ID)
+	return err
+}
+
 // AuthorityCert returns the SKS cluster base64-encoded certificate content for the specified authority.
 func (c *SKSCluster) AuthorityCert(ctx context.Context, authority string) (string, error) {
 	if authority == "" {

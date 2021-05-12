4f3115e9126489eb492fe7ce0a54c2ef0d0a7b80
Marc Falzon <marc@exoscale.com>
2021-05-12 12:33:48 +0200

    v2: add new InstanceType type
    This change adds support for Compute instance types via the new type
    `InstanceType`.

---
+++ v2/instance_type_test.go
@@ -0,0 +1,74 @@
+package v2
+
+import (
+	"context"
+	"fmt"
+
+	papi "github.com/exoscale/egoscale/v2/internal/public-api"
+)
+
+var (
+	testInstanceTypeAuthorized       = true
+	testInstanceTypeCPUs       int64 = 16
+	testInstanceTypeGPUs       int64 = 2
+	testInstanceTypeFamily           = "gpu2"
+	testInstanceTypeID               = new(clientTestSuite).randomID()
+	testInstanceTypeMemory     int64 = 96636764160
+	testInstanceTypeSize             = "medium"
+)
+
+func (ts *clientTestSuite) TestClient_ListInstanceTypes() {
+	ts.mockAPIRequest("GET", "/instance-type", struct {
+		InstanceTypes *[]papi.InstanceType `json:"instance-types,omitempty"`
+	}{
+		InstanceTypes: &[]papi.InstanceType{{
+			Authorized: &testInstanceTypeAuthorized,
+			Cpus:       &testInstanceTypeCPUs,
+			Family:     &testInstanceTypeFamily,
+			Gpus:       &testInstanceTypeGPUs,
+			Id:         &testInstanceTypeID,
+			Memory:     &testInstanceTypeMemory,
+			Size:       &testInstanceTypeSize,
+		}},
+	})
+
+	expected := []*InstanceType{{
+		Authorized: testInstanceTypeAuthorized,
+		CPUs:       testInstanceTypeCPUs,
+		Family:     testInstanceTypeFamily,
+		GPUs:       testInstanceTypeGPUs,
+		ID:         testInstanceTypeID,
+		Memory:     testInstanceTypeMemory,
+		Size:       testInstanceTypeSize,
+	}}
+
+	actual, err := ts.client.ListInstanceTypes(context.Background(), testZone)
+	ts.Require().NoError(err)
+	ts.Require().Equal(expected, actual)
+}
+
+func (ts *clientTestSuite) TestClient_GetInstanceType() {
+	ts.mockAPIRequest("GET", fmt.Sprintf("/instance-type/%s", testInstanceTypeID), papi.InstanceType{
+		Authorized: &testInstanceTypeAuthorized,
+		Cpus:       &testInstanceTypeCPUs,
+		Family:     &testInstanceTypeFamily,
+		Gpus:       &testInstanceTypeGPUs,
+		Id:         &testInstanceTypeID,
+		Memory:     &testInstanceTypeMemory,
+		Size:       &testInstanceTypeSize,
+	})
+
+	expected := &InstanceType{
+		Authorized: testInstanceTypeAuthorized,
+		CPUs:       testInstanceTypeCPUs,
+		Family:     testInstanceTypeFamily,
+		GPUs:       testInstanceTypeGPUs,
+		ID:         testInstanceTypeID,
+		Memory:     testInstanceTypeMemory,
+		Size:       testInstanceTypeSize,
+	}
+
+	actual, err := ts.client.GetInstanceType(context.Background(), testZone, expected.ID)
+	ts.Require().NoError(err)
+	ts.Require().Equal(expected, actual)
+}

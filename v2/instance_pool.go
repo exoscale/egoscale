dd7af53ceb8da27401369aecf60b996e033e7417
Philippe Chepy <philippe.chepy@exoscale.ch>
2022-04-19 11:13:16 +0200

    go fmt

--- v2/instance_pool.go
+++ v2/instance_pool.go
@@ -231,7 +231,7 @@ func (c *Client) CreateInstancePool(
 
 	instancePoolID := *res.(*struct {
 		Command *string `json:"command,omitempty"`
-		Id *string `json:"id,omitempty"` // revive:disable-line
+		Id      *string `json:"id,omitempty"` // revive:disable-line
 		Link    *string `json:"link,omitempty"`
 	}).Id
 

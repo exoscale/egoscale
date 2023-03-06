fa12d68cf5862ab80bb8408e027c8117332417b0
Philipp Sauter <philipp.sauter@exoscale.ch>
2023-03-06 16:07:39 +0100

    fix linter issue

--- admin/doc.go
+++ admin/doc.go
@@ -5,4 +5,5 @@ Package admin contains the privileged API calls.
 Some aspects of the Exoscale API are restricted to admin privileges, meaning not all field would be useful for everyone if we allowed them to coexist with the base structs. E.g. an admin can list all resources belonging to anyone using the listall=true parameter.
 
 */
+//nolint:goimports
 package admin

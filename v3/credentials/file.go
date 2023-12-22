4d12fb8acd79787e19f7809cfb7cf5c2a8c91241
Pierre-Emmanuel Jacquier <15922119+pierre-emmanuelJ@users.noreply.github.com>
2023-12-22 16:07:07 +0000

    Misc
    Signed-off-by: Pierre-Emmanuel Jacquier <15922119+pierre-emmanuelJ@users.noreply.github.com>

--- v3/credentials/file.go
+++ v3/credentials/file.go
@@ -1,16 +1,42 @@
 package credentials
 
-type FileProvider struct {
+import "fmt"
+
+type FileOpt func(*FileProvider)
+
+// FileOptWithFilename returns a FileOpt overriding the default filename.
+func FileOptWithFilename(filename string) FileOpt {
+	return func(f *FileProvider) {
+		f.filename = filename
+	}
 }
 
-func NewFileCredentials() *Credentials {
-	return NewCredentials(&FileProvider{})
+// FileOptWithAccount returns a FileOpt overriding the default account.
+func FileOptWithAccount(account string) FileOpt {
+	return func(f *FileProvider) {
+		f.account = account
+	}
+}
+
+type FileProvider struct {
+	filename  string
+	account   string
+	retrieved bool
+}
+
+func NewFileCredentials(opts ...FileOpt) *Credentials {
+	fp := &FileProvider{}
+	for _, opt := range opts {
+		opt(fp)
+	}
+	return NewCredentials(fp)
 }
 
 func (f *FileProvider) Retrieve() (Value, error) {
-	return Value{}, nil
+	return Value{}, fmt.Errorf("not implemented")
 }
 
-func (f *FileProvider) IsExpired() bool {
-	return false
+// IsExpired returns if the shared credentials have expired.
+func (p *FileProvider) IsExpired() bool {
+	return !p.retrieved
 }

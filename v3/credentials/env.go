7c6115a4715ce04f2e82889530eb5fbb72ea1c74
Pierre-Emmanuel Jacquier <15922119+pierre-emmanuelJ@users.noreply.github.com>
2023-12-22 15:44:31 +0000

    Add some implem in provider side
    Signed-off-by: Pierre-Emmanuel Jacquier <15922119+pierre-emmanuelJ@users.noreply.github.com>

--- v3/credentials/env.go
+++ v3/credentials/env.go
@@ -1,16 +1,34 @@
 package credentials
 
+import "os"
+
 type EnvProvider struct {
+	retrieved bool
 }
 
 func NewEnvCredentials() *Credentials {
 	return NewCredentials(&EnvProvider{})
 }
 
+// Retrieve retrieves the keys from the environment.
 func (e *EnvProvider) Retrieve() (Value, error) {
-	return Value{}, nil
+	e.retrieved = false
+
+	key := os.Getenv("AWS_ACCESS_KEY_ID")
+	secret := os.Getenv("AWS_SECRET_ACCESS_KEY")
+
+	if key == "" || secret == "" {
+		return Value{}, ErrMissingIncomplete
+	}
+
+	e.retrieved = true
+	return Value{
+		APIKey:    key,
+		APISecret: secret,
+	}, nil
 }
 
+// IsExpired returns if the credentials have been retrieved.
 func (e *EnvProvider) IsExpired() bool {
-	return false
+	return !e.retrieved
 }

827e2c7b474944b480ef2e48bbb09941a6bb42b2
Pierre-Emmanuel Jacquier <15922119+pierre-emmanuelJ@users.noreply.github.com>
2024-05-02 14:21:29 +0000

    v3: Add a metadata package to interact with
    Signed-off-by: Pierre-Emmanuel Jacquier <15922119+pierre-emmanuelJ@users.noreply.github.com>

---
+++ v3/metadata/metadata.go
@@ -0,0 +1,61 @@
+package metadata
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"net/url"
+)
+
+type Endpoint string
+
+const (
+	AvailabilityZone Endpoint = "availability-zone"
+	CloudIdentifier  Endpoint = "cloud-identifier"
+	InstanceID       Endpoint = "instance-id"
+	LocalHostname    Endpoint = "local-hostname"
+	LocalIpv4        Endpoint = "local-ipv4"
+	PublicHostname   Endpoint = "public-hostname"
+	PublicIpv4       Endpoint = "public-ipv4"
+	ServiceOffering  Endpoint = "service-offering"
+	VmID             Endpoint = "vm-id"
+)
+
+const (
+	URL         = "http://metadata.exoscale.com/latest/"
+	MetaDataURL = URL + "meta-data"
+	UserDataURL = URL + "user-data"
+)
+
+func UserData(ctx context.Context) (string, error) {
+	return httpGet(ctx, UserDataURL)
+}
+
+func Get(ctx context.Context, endpoint Endpoint) (string, error) {
+	url, err := url.JoinPath(MetaDataURL, string(endpoint))
+	if err != nil {
+		return "", err
+	}
+
+	return httpGet(ctx, url)
+}
+
+func httpGet(ctx context.Context, url string) (string, error) {
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+	if err != nil {
+		return "", err
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(body), nil
+}

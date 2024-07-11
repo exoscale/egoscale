01801dd563e17c4b8781a01dc54d4d82893bd5cc
Pierre-Emmanuel Jacquier <15922119+pierre-emmanuelJ@users.noreply.github.com>
2024-07-11 16:51:52 +0000

    Change log
    Signed-off-by: Pierre-Emmanuel Jacquier <15922119+pierre-emmanuelJ@users.noreply.github.com>

--- v3/errors.go
+++ v3/errors.go
@@ -8,36 +8,35 @@ import (
 )
 
 var (
-	ErrBadRequest                   = fmt.Errorf(http.StatusText(http.StatusBadRequest))
-	ErrUnauthorized                 = fmt.Errorf(http.StatusText(http.StatusUnauthorized))
-	ErrPaymentRequired              = fmt.Errorf(http.StatusText(http.StatusPaymentRequired))
-	ErrForbidden                    = fmt.Errorf(http.StatusText(http.StatusForbidden))
-	ErrNotFound                     = fmt.Errorf(http.StatusText(http.StatusNotFound))
-	ErrMethodNotAllowed             = fmt.Errorf(http.StatusText(http.StatusMethodNotAllowed))
-	ErrNotAcceptable                = fmt.Errorf(http.StatusText(http.StatusNotAcceptable))
-	ErrProxyAuthRequired            = fmt.Errorf(http.StatusText(http.StatusProxyAuthRequired))
-	ErrRequestTimeout               = fmt.Errorf(http.StatusText(http.StatusRequestTimeout))
-	ErrConflict                     = fmt.Errorf(http.StatusText(http.StatusConflict))
-	ErrGone                         = fmt.Errorf(http.StatusText(http.StatusGone))
-	ErrLengthRequired               = fmt.Errorf(http.StatusText(http.StatusLengthRequired))
-	ErrPreconditionFailed           = fmt.Errorf(http.StatusText(http.StatusPreconditionFailed))
-	ErrRequestEntityTooLarge        = fmt.Errorf(http.StatusText(http.StatusRequestEntityTooLarge))
-	ErrRequestURITooLong            = fmt.Errorf(http.StatusText(http.StatusRequestURITooLong))
-	ErrUnsupportedMediaType         = fmt.Errorf(http.StatusText(http.StatusUnsupportedMediaType))
-	ErrRequestedRangeNotSatisfiable = fmt.Errorf(http.StatusText(http.StatusRequestedRangeNotSatisfiable))
-	ErrExpectationFailed            = fmt.Errorf(http.StatusText(http.StatusExpectationFailed))
-	ErrTeapot                       = fmt.Errorf(http.StatusText(http.StatusTeapot))
-	ErrMisdirectedRequest           = fmt.Errorf(http.StatusText(http.StatusMisdirectedRequest))
-	ErrUnprocessableEntity          = fmt.Errorf(http.StatusText(http.StatusUnprocessableEntity))
-	ErrLocked                       = fmt.Errorf(http.StatusText(http.StatusLocked))
-	ErrFailedDependency             = fmt.Errorf(http.StatusText(http.StatusFailedDependency))
-	ErrTooEarly                     = fmt.Errorf(http.StatusText(http.StatusTooEarly))
-	ErrUpgradeRequired              = fmt.Errorf(http.StatusText(http.StatusUpgradeRequired))
-	ErrPreconditionRequired         = fmt.Errorf(http.StatusText(http.StatusPreconditionRequired))
-	ErrTooManyRequests              = fmt.Errorf(http.StatusText(http.StatusTooManyRequests))
-	ErrRequestHeaderFieldsTooLarge  = fmt.Errorf(http.StatusText(http.StatusRequestHeaderFieldsTooLarge))
-	ErrUnavailableForLegalReasons   = fmt.Errorf(http.StatusText(http.StatusUnavailableForLegalReasons))
-
+	ErrBadRequest                    = fmt.Errorf(http.StatusText(http.StatusBadRequest))
+	ErrUnauthorized                  = fmt.Errorf(http.StatusText(http.StatusUnauthorized))
+	ErrPaymentRequired               = fmt.Errorf(http.StatusText(http.StatusPaymentRequired))
+	ErrForbidden                     = fmt.Errorf(http.StatusText(http.StatusForbidden))
+	ErrNotFound                      = fmt.Errorf(http.StatusText(http.StatusNotFound))
+	ErrMethodNotAllowed              = fmt.Errorf(http.StatusText(http.StatusMethodNotAllowed))
+	ErrNotAcceptable                 = fmt.Errorf(http.StatusText(http.StatusNotAcceptable))
+	ErrProxyAuthRequired             = fmt.Errorf(http.StatusText(http.StatusProxyAuthRequired))
+	ErrRequestTimeout                = fmt.Errorf(http.StatusText(http.StatusRequestTimeout))
+	ErrConflict                      = fmt.Errorf(http.StatusText(http.StatusConflict))
+	ErrGone                          = fmt.Errorf(http.StatusText(http.StatusGone))
+	ErrLengthRequired                = fmt.Errorf(http.StatusText(http.StatusLengthRequired))
+	ErrPreconditionFailed            = fmt.Errorf(http.StatusText(http.StatusPreconditionFailed))
+	ErrRequestEntityTooLarge         = fmt.Errorf(http.StatusText(http.StatusRequestEntityTooLarge))
+	ErrRequestURITooLong             = fmt.Errorf(http.StatusText(http.StatusRequestURITooLong))
+	ErrUnsupportedMediaType          = fmt.Errorf(http.StatusText(http.StatusUnsupportedMediaType))
+	ErrRequestedRangeNotSatisfiable  = fmt.Errorf(http.StatusText(http.StatusRequestedRangeNotSatisfiable))
+	ErrExpectationFailed             = fmt.Errorf(http.StatusText(http.StatusExpectationFailed))
+	ErrTeapot                        = fmt.Errorf(http.StatusText(http.StatusTeapot))
+	ErrMisdirectedRequest            = fmt.Errorf(http.StatusText(http.StatusMisdirectedRequest))
+	ErrUnprocessableEntity           = fmt.Errorf(http.StatusText(http.StatusUnprocessableEntity))
+	ErrLocked                        = fmt.Errorf(http.StatusText(http.StatusLocked))
+	ErrFailedDependency              = fmt.Errorf(http.StatusText(http.StatusFailedDependency))
+	ErrTooEarly                      = fmt.Errorf(http.StatusText(http.StatusTooEarly))
+	ErrUpgradeRequired               = fmt.Errorf(http.StatusText(http.StatusUpgradeRequired))
+	ErrPreconditionRequired          = fmt.Errorf(http.StatusText(http.StatusPreconditionRequired))
+	ErrTooManyRequests               = fmt.Errorf(http.StatusText(http.StatusTooManyRequests))
+	ErrRequestHeaderFieldsTooLarge   = fmt.Errorf(http.StatusText(http.StatusRequestHeaderFieldsTooLarge))
+	ErrUnavailableForLegalReasons    = fmt.Errorf(http.StatusText(http.StatusUnavailableForLegalReasons))
 	ErrInternalServerError           = fmt.Errorf(http.StatusText(http.StatusInternalServerError))
 	ErrNotImplemented                = fmt.Errorf(http.StatusText(http.StatusNotImplemented))
 	ErrBadGateway                    = fmt.Errorf(http.StatusText(http.StatusBadGateway))
@@ -52,36 +51,35 @@ var (
 )
 
 var httpStatusCodeErrors = map[int]error{
-	http.StatusBadRequest:                   ErrBadRequest,
-	http.StatusUnauthorized:                 ErrUnauthorized,
-	http.StatusPaymentRequired:              ErrPaymentRequired,
-	http.StatusForbidden:                    ErrForbidden,
-	http.StatusNotFound:                     ErrNotFound,
-	http.StatusMethodNotAllowed:             ErrMethodNotAllowed,
-	http.StatusNotAcceptable:                ErrNotAcceptable,
-	http.StatusProxyAuthRequired:            ErrProxyAuthRequired,
-	http.StatusRequestTimeout:               ErrRequestTimeout,
-	http.StatusConflict:                     ErrConflict,
-	http.StatusGone:                         ErrGone,
-	http.StatusLengthRequired:               ErrLengthRequired,
-	http.StatusPreconditionFailed:           ErrPreconditionFailed,
-	http.StatusRequestEntityTooLarge:        ErrRequestEntityTooLarge,
-	http.StatusRequestURITooLong:            ErrRequestURITooLong,
-	http.StatusUnsupportedMediaType:         ErrUnsupportedMediaType,
-	http.StatusRequestedRangeNotSatisfiable: ErrRequestedRangeNotSatisfiable,
-	http.StatusExpectationFailed:            ErrExpectationFailed,
-	http.StatusTeapot:                       ErrTeapot,
-	http.StatusMisdirectedRequest:           ErrMisdirectedRequest,
-	http.StatusUnprocessableEntity:          ErrUnprocessableEntity,
-	http.StatusLocked:                       ErrLocked,
-	http.StatusFailedDependency:             ErrFailedDependency,
-	http.StatusTooEarly:                     ErrTooEarly,
-	http.StatusUpgradeRequired:              ErrUpgradeRequired,
-	http.StatusPreconditionRequired:         ErrPreconditionRequired,
-	http.StatusTooManyRequests:              ErrTooManyRequests,
-	http.StatusRequestHeaderFieldsTooLarge:  ErrRequestHeaderFieldsTooLarge,
-	http.StatusUnavailableForLegalReasons:   ErrUnavailableForLegalReasons,
-
+	http.StatusBadRequest:                    ErrBadRequest,
+	http.StatusUnauthorized:                  ErrUnauthorized,
+	http.StatusPaymentRequired:               ErrPaymentRequired,
+	http.StatusForbidden:                     ErrForbidden,
+	http.StatusNotFound:                      ErrNotFound,
+	http.StatusMethodNotAllowed:              ErrMethodNotAllowed,
+	http.StatusNotAcceptable:                 ErrNotAcceptable,
+	http.StatusProxyAuthRequired:             ErrProxyAuthRequired,
+	http.StatusRequestTimeout:                ErrRequestTimeout,
+	http.StatusConflict:                      ErrConflict,
+	http.StatusGone:                          ErrGone,
+	http.StatusLengthRequired:                ErrLengthRequired,
+	http.StatusPreconditionFailed:            ErrPreconditionFailed,
+	http.StatusRequestEntityTooLarge:         ErrRequestEntityTooLarge,
+	http.StatusRequestURITooLong:             ErrRequestURITooLong,
+	http.StatusUnsupportedMediaType:          ErrUnsupportedMediaType,
+	http.StatusRequestedRangeNotSatisfiable:  ErrRequestedRangeNotSatisfiable,
+	http.StatusExpectationFailed:             ErrExpectationFailed,
+	http.StatusTeapot:                        ErrTeapot,
+	http.StatusMisdirectedRequest:            ErrMisdirectedRequest,
+	http.StatusUnprocessableEntity:           ErrUnprocessableEntity,
+	http.StatusLocked:                        ErrLocked,
+	http.StatusFailedDependency:              ErrFailedDependency,
+	http.StatusTooEarly:                      ErrTooEarly,
+	http.StatusUpgradeRequired:               ErrUpgradeRequired,
+	http.StatusPreconditionRequired:          ErrPreconditionRequired,
+	http.StatusTooManyRequests:               ErrTooManyRequests,
+	http.StatusRequestHeaderFieldsTooLarge:   ErrRequestHeaderFieldsTooLarge,
+	http.StatusUnavailableForLegalReasons:    ErrUnavailableForLegalReasons,
 	http.StatusInternalServerError:           ErrInternalServerError,
 	http.StatusNotImplemented:                ErrNotImplemented,
 	http.StatusBadGateway:                    ErrBadGateway,

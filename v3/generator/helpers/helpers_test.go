d380d780e8c4f88642af27b0e6ba765917a9eb47
simisoft-exo <108657152+simisoft-exo@users.noreply.github.com>
2024-08-16 13:41:14 +0100

    handling multiple separators (#650)
    # Description
    Generator would break with the [new DBaaS enums for
    Datadog](https://github.com/exoscale/dbaas-api/blob/40b1ba53298095740a736f55905f3e4aff62a542/modules/dbaas-schemas/resources/schemas/dbaas-api.edn#L2562)
    
    ```clojure
      :enum-datadog-site
      #{"us3.datadoghq.com"
        "ddog-gov.com"
        "datadoghq.eu"
        "us5.datadoghq.com"
        "ap1.datadoghq.com"
        "datadoghq.com"}
    ```
    For the entry "ddog-gov.com" the existing code considers only one
    separator and it generates
    ```go
    EnumDatadogSiteDdogGov.com      EnumDatadogSite = "ddog-gov.com"
    ```
    
    With these changes it would generate 
    ```go
    EnumDatadogSiteDdogGovCom      EnumDatadogSite = "ddog-gov.com"
    ```
    
    ## Checklist
    (For exoscale contributors)
    
    * [x] Changelog updated (under *Unreleased* block)
    * [x] For a new resource or new attributes: test added/updated
    
    ---------
    
    Co-authored-by: Philipp Sauter <philipp.sauter@exoscale.ch>

--- v3/generator/helpers/helpers_test.go
+++ v3/generator/helpers/helpers_test.go
@@ -13,6 +13,30 @@ func TestToCamel(t *testing.T) {
 	require.Equal(t, "Test", ToCamel("test"))
 	require.Equal(t, "", ToCamel("."))
 	require.Equal(t, "TestFoo", ToCamel("test...foo"))
+
+	// trimming
+	require.Equal(t, "Test", ToCamel("---test"))
+	require.Equal(t, "Test", ToCamel("test___"))
+	require.Equal(t, "Test", ToCamel(".-_test_-."))
+	require.Equal(t, "Test", ToCamel("  test  "))
+	require.Equal(t, "Test", ToCamel(" .-_test_-. "))
+
+	// splitting
+	require.Equal(t, "TestFooBarBaz", ToCamel("test-foo_bar.baz"))
+	require.Equal(t, "TestFooBar", ToCamel("test--_foo..bar"))
+	require.Equal(t, "TESTFooBAR", ToCamel("TEST-Foo-BAR"))
+	require.Equal(t, "Test123Foo", ToCamel("test-123-foo"))
+	require.Equal(t, "123", ToCamel("1-2-3"))
+
+	// trimming and splitting
+	require.Equal(t, "TrimMixedSeparators", ToCamel("-_. trim-mixed/separators ._-"))
+
+	// Acronym handling
+	require.Equal(t, "HandleXMLHTTPRequest", ToCamel("handle-xml-http-request"))
+	require.Equal(t, "ParseJSONAPI", ToCamel("parse_json_api"))
+	require.Equal(t, "NewURLForID", ToCamel("new-url-for-id"))
+	require.Equal(t, "SupportSQLQueries", ToCamel("support-sql-queries"))
+
 }
 
 func TestToLowerCamel(t *testing.T) {
